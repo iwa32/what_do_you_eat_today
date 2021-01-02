a00da3365cf7df3a641bf188bfdc7e6bc559a121
iwai32 <iwakou32@gmail.com>
2021-01-02 23:03:45 +0900

    ログイン保持のチェックボックスを追加

--- login.php
+++ login.php
@@ -1,3 +1,71 @@
+<?php
+
+require('function.php');
+
+//デバッグ
+debug('「「「「「「「「「「「「「「「「');
+debug('ログインページ');
+
+debugLogStart();
+
+//認証チェック
+require('auth.php');
+
+//ログイン処理
+if (!empty($_POST)) {
+
+  debug('POST送信されました。');
+  $email = $_POST['email'];
+  $password = $_POST['password'];
+
+  //バリデーション
+  debug('バリデーション開始');
+  //email
+  validRequired($email, 'email');
+  validMaxLen($email, 'email');
+  validTypeEmail($email);
+  //password
+  validRequired($password, 'password');
+  validMinLen($password, 'password');
+  validHalf($password, 'password');
+
+  if (empty($err_msg)) {
+
+    debug('バリデーションOK');
+    try {
+      $sql = 'SELECT password,id FROM users WHERE email = :email';
+      $pdo = dbConnect();
+      $data = [
+        ':email' => $email
+      ];
+
+      $stmt = queryPost($pdo, $sql, $data);
+      $result = $stmt->fetch(PDO::FETCH_ASSOC);
+      debug('resultの中身' . print_r($result, true));
+
+      if (!empty($result) && password_verify($password, $result['password'])) {
+        //パスワードの照合
+
+        debug('パスワードが一致しました。');
+        
+      } else {
+
+        debug('パスワードが一致しませんでした。');
+        $err_msg['common'] = MSG_NO_MATCH;
+      }
+
+    } catch(Exception $e) {
+      debug('システムエラー' . $e->getMessage());
+      $err_msg['common'] = MSG_SYS_ERROR;
+    }
+    //セッションにデータを保存
+    //リダイレクト
+  }
+}
+
+?>
+
+
 <?php require("head.php") ?>
 
 <body>
@@ -8,13 +76,16 @@
       <div class="form-area">
         <h2 class="form-area__title">ログイン</h2>
 
-        <form action="" method="POST">
+        <form action="" method="POST" id="loginForm">
+          <div class="form-area__group">
+            <div class="form-area__group__alert"><?php if (!empty($err_msg['common'])) echo $err_msg['common'] ?></div>
+          </div>
           <div class="form-area__group">
             <label for="email">
               <div class="form-area__group__name">Eメール<span class="form-area__group__badge form-area__group__badge--required">[必須]</span></div>
               <div class="form-area__group__help">Eメール形式で入力してください</div>
-              <input class="form-area__group__input" type="text" name="email" id="email">
-              <div class="form-area__group__alert">エラーテキスト</div>
+              <input class="form-area__group__input" type="text" name="email" id="email"　value="<?php if(!empty($_POST['email'])) echo $_POST['email']; ?>">
+              <div class="form-area__group__alert"><?php if(!empty($err_msg['email'])) echo $err_msg['email']; ?></div>
               <div class="form-area__group__place-holder">you@example.com</div>
             </label>
           </div>
@@ -23,21 +94,26 @@
             <label for="password">
               <div class="form-area__group__name">パスワード<span class="form-area__group__badge form-area__group__badge--required">[必須]</span></div>
               <div class="form-area__group__help">パスワードは5文字以上で入力してください</div>
-              <input class="form-area__group__input" type="password" name="password" id="password">
-              <div class="form-area__group__alert">エラーテキスト</div>
+              <input class="form-area__group__input" type="password" name="password" id="password" value="<?php if(!empty($_POST['password'])) echo $_POST['password']; ?>">
+              <div class="form-area__group__alert"><?php if(!empty($err_msg['password'])) echo $err_msg['password']; ?></div>
+            </label>
+          </div>
+
+          <div class="form-area__group">
+            <label for="password_save" class="form-area__group__checkbox-area">
+              <input class="form-area__group__check" type="checkbox" name="password_save" id="password_save">
+              <div class="form-area__group__name">ログインを保持する。</div>
             </label>
           </div>
 
           <div class="form-area__btn-group">
             <div class="form-area__btn--wrapp">
-              <button class="form-area__btn form-area__btn--normal">ログインする</button>
+              <button class="form-area__btn form-area__btn--normal disabled" id="submitBtn">ログインする</button>
             </div>
 
             <div class="form-area__btn--wrapp">
               <button class="form-area__btn form-area__btn--twitter">Sign in with Twitter</button>
             </div>
-
-
           </div>
         </form>
 
