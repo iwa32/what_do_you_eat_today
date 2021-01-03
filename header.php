94cd533da0b7f73db1adaadf355beab59b4726f1
iwai32 <iwakou32@gmail.com>
2021-01-04 02:47:10 +0900

    認証済みかどうかでメニューを切り替える

--- header.php
+++ header.php
@@ -2,16 +2,19 @@
   <div class="g-header__wrapp container">
     <h1 class="g-header__logo"><a href="./">今日は何食べる？</a></h1>
     <nav class="g-header__nav">
-      <!-- <ul>
-        <li><a href="mypage.php">マイページ</a></li>
-        <li><a href="">ログアウト</a></li>
-        <li><a href="">退会</a></li>
-      </ul> -->
-
       <ul>
-          <li><a href="./login.php">ログイン</a></li>
-          <li><a href="./signup.php">ユーザー登録</a></li>
-        </ul>
+      <?php
+      if (empty($_SESSION['user_id'])) {
+      ?>
+        <li><a href="./login.php">ログイン</a></li>
+        <li><a href="./signup.php">ユーザー登録</a></li>
+      <?php
+      } else {
+      ?>
+        <li><a href="./my_page.php">マイページ</a></li>
+        <li><a href="./logout.php">ログアウト</a></li>
+      <?php } ?>
+      </ul>
     </nav>
   </div>
 </header>
