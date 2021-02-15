5653c51d5aba2e445ff5b8ba6221f250bfb9f4f3
iwai32 <iwakou32@gmail.com>
2021-02-15 20:35:52 +0900

    存在チェックを関数にまとめて表示に変更

--- signup.php
+++ signup.php
@@ -29,7 +29,7 @@ if (!empty($_POST)) {
 
       try {
         $pdo = dbConnect();
-        $sql = 'INSERT INTO users (name, email, password) VALUES (:name, :email, :password) WHERE deleted_at IS NULL';
+        $sql = 'INSERT INTO users (name, email, password) VALUES (:name, :email, :password)';
 
         $data = [
           ':name' => $name,
@@ -79,15 +79,15 @@ require("head.php");
       <div class="form-area">
         <h2 class="form-area__title">ユーザー登録</h2>
 
-        <form action="" method="POST" id="signUpForm">
+        <form action="" method="POST" id="authForm">
           <div class="form-area__group">
-            <div class="form-area__group__alert"><?php if (!empty($err_msg['common'])) echo $err_msg['common'] ?></div>
+            <div class="form-area__group__alert"><?php echo getErrMsg('common'); ?></div>
           </div>
           <div class="form-area__group">
             <label for="name">
               <div class="form-area__group__name">お名前<span class="form-area__group__badge form-area__group__badge--any">[任意]</span></div>
               <input class="form-area__group__input" type="text" name="name" id="name" value="<?php if(!empty($_POST['name'])) echo $_POST['name']; ?>">
-              <div class="form-area__group__alert"><?php if (!empty($err_msg['name'])) echo $err_msg['name']; ?></div>
+              <div class="form-area__group__alert"><?php echo getErrMsg('name') ?></div>
               <div class="form-area__group__place-holder">山田太郎</div>
             </label>
           </div>
@@ -97,7 +97,7 @@ require("head.php");
               <div class="form-area__group__name">Eメール<span class="form-area__group__badge form-area__group__badge--required">[必須]</span></div>
               <div class="form-area__group__help">Eメール形式で入力してください</div>
               <input class="form-area__group__input" type="text" name="email" id="email" value="<?php if(!empty($_POST['email'])) echo $_POST['email']; ?>">
-              <div class="form-area__group__alert"><?php if (!empty($err_msg['email'])) echo $err_msg['email']; ?></div>
+              <div class="form-area__group__alert"><?php echo getErrMsg('email'); ?></div>
               <div class="form-area__group__place-holder">you@example.com</div>
             </label>
           </div>
@@ -107,17 +107,17 @@ require("head.php");
               <div class="form-area__group__name">パスワード<span class="form-area__group__badge form-area__group__badge--required">[必須]</span></div>
               <div class="form-area__group__help">5文字以上で半角英数字で入力してください</div>
               <input class="form-area__group__input" type="password" name="password" id="password" value="<?php if(!empty($_POST['password'])) echo $_POST['password'] ?>">
-              <div class="form-area__group__alert"><?php if (!empty($err_msg['password'])) echo $err_msg['password'] ?></div>
+              <div class="form-area__group__alert"><?php echo getErrMsg('password'); ?></div>
             </label>
           </div>
 
           <div class="form-area__btn-group">
             <div class="form-area__btn--wrapp">
-              <button class="form-area__btn form-area__btn--normal disabled" id="submitBtn">登録する</button>
+              <button class="form-area__auth-btn form-area__auth-btn--normal disabled" id="submitBtn">登録する</button>
             </div>
 
             <div class="form-area__btn--wrapp">
-              <button class="form-area__btn form-area__btn--twitter">Sign in with Twitter</button>
+              <button class="form-area__auth-btn form-area__auth-btn--twitter">Sign in with Twitter</button>
             </div>
           </div>
         </form>
