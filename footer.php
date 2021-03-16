f766efa7755d497b69b94cd2b7b6a603241fb016
iwai32 <iwakou32@gmail.com>
2021-03-16 09:17:01 +0900

    google api places api使用のためのscriptを記述。API_KEYのアップは検討中

--- footer.php
+++ footer.php
@@ -3,7 +3,12 @@
     <small>Copyright &copy; 2020-2020 今日は何食べる？<br class="sp-on"> All Rights Reserved.</small>
   </div>
 </footer>
+<script src="http://maps.google.com/maps/api/js?key={API_KEY}&libraries=places&language=ja"></script>
 <script src = "./node_modules/jquery/dist/jquery.js"></script>
+<script>
+  //お気に入り登録時とお気に入りアイコン表示の有無のために使用する
+  var userId = '<?php echo (!empty($_SESSION['user_id'])) ? $_SESSION['user_id']: ''; ?>';
+</script>
 <script src="./js/index.js"></script>
 <script src="./js/const.js"></script>
 <script src="./js/function.js"></script>
